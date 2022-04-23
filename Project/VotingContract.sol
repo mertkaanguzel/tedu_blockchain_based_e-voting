f8a314e334fa1fcce2cf702cadb3f1ca7caa2c50
ecemergul <ecemergul@gmail.com>
2022-04-23 15:57:34 +0300

    v 0.0.0.1

---
+++ Project/VotingContract.sol
@@ -0,0 +1,70 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract VotingContract {
+    
+    address public admin;
+
+    struct Party {
+        string partyName;
+        uint numberOfVotes;
+        // string url;
+    }   
+
+    struct VoterUser {
+        uint votedParty;
+        bool votingState; //if false didn't vote if true voted
+        bool canVote;
+    }
+
+    mapping (address => VoterUser) public voters;
+
+    Party[] public parties;
+
+    constructor(string[] memory partyNames) {
+        admin = msg.sender;
+
+        for(uint i = 0; i < partyName.length; i++) {
+            parties.push(Party({
+                partyName: partyNames[i],
+                numberOfVotes: 0
+            }));
+        }
+    }
+
+    function giveVote(uint partyIndex) external {
+        VoterUser storage caller = voters[msg.sender];
+        require(!caller.canVote);
+        require(!caller.votingState);
+        caller.votingState = true;
+        caller.votedParty = partyIndex;
+        parties[partyIndex].numberOfVotes += 1; 
+    }
+
+    function declareWinner() external view
+        returns(string winner)
+    {
+        uint winningIndex;
+        uint voteCounter = 0;
+        for(uint i = 0; i < parties.length; i++) {
+            if(parties[i].numberOfVotes > voteCounter) {
+                voteCounter = parties[i].numberOfVotes;
+                winningIndex = i;
+            }
+        }
+
+        winner = parties[winningIndex].partyName; 
+    }
+
+    function makeEligibleToVote(address VoterUser) external {
+        require(
+            msg.sender == admin;
+        );
+        require(
+            !voters[VoterUser].votingState 
+        );
+        if(!voters[VoterUser].canVote) {
+            voters[VoterUser].canVote = true;
+        }   
+    } 
+
+}
